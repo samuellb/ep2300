d0a2651ddcc349fd530abf82aa66315e1946659a
Samuel Lidén Borell <samuel@slbdata.se>
2011-09-27 19:23:53 +0200

    Factored out the Snmp session creation code

--- src/ep2300/Test.java
+++ src/ep2300/Test.java
@@ -1,34 +1,19 @@
 package ep2300;
 
-import com.adventnet.snmp.snmp2.ProtocolOptions;
 import com.adventnet.snmp.snmp2.SnmpAPI;
-import com.adventnet.snmp.snmp2.SnmpEngineEntry;
 import com.adventnet.snmp.snmp2.SnmpOID;
 import com.adventnet.snmp.snmp2.SnmpPDU;
 import com.adventnet.snmp.snmp2.SnmpSession;
-import com.adventnet.snmp.snmp2.UDPProtocolOptions;
-import com.adventnet.snmp.snmp2.usm.USMUserEntry;
-import com.adventnet.snmp.snmp2.usm.USMUtils;
 
 
 public class Test {
     
     public static void main(String[] args) throws Exception {
         
-        SnmpAPI api = new SnmpAPI();
-        ProtocolOptions protocolOptions = new UDPProtocolOptions("192.168.1.10");
-        
-        SnmpSession session = new SnmpSession(api);
-        session.setVersion(SnmpAPI.SNMP_VERSION_3);
-        session.setProtocolOptions(protocolOptions);
-        //session.setUserName("2G1332_student".getBytes());
-        session.open();
-        
-        USMUtils.init_v3_parameters("2G1332_student", null, USMUserEntry.MD5_AUTH,
-            "netmanagement", null, protocolOptions, session, true);
+        SnmpSession session = UDPSnmpV3.createSession("192.168.1.10",
+            "2G1332_student", "netmanagement");
         
         SnmpPDU pdu = new SnmpPDU();
-//        pdu.setProtocolOptions(protocolOptions);
         pdu.setCommand(SnmpAPI.GET_REQ_MSG);
         pdu.addNull(new SnmpOID(".1.3.6.1.2.1.1.1.0"));
         SnmpPDU response_pdu = session.syncSend(pdu);
---
+++ src/ep2300/UDPSnmpV3.java
@@ -0,0 +1,38 @@
+package ep2300;
+
+import com.adventnet.snmp.snmp2.ProtocolOptions;
+import com.adventnet.snmp.snmp2.SnmpAPI;
+import com.adventnet.snmp.snmp2.SnmpException;
+import com.adventnet.snmp.snmp2.SnmpSession;
+import com.adventnet.snmp.snmp2.UDPProtocolOptions;
+import com.adventnet.snmp.snmp2.usm.USMUserEntry;
+import com.adventnet.snmp.snmp2.usm.USMUtils;
+
+public class UDPSnmpV3 {
+    
+    private UDPSnmpV3() { } // static class
+    
+    private static SnmpAPI api = new SnmpAPI();
+    
+    /**
+     * Creates an SNMPv3 session with the given username and password for
+     * authorization (encryption is not used).
+     */
+    public static SnmpSession createSession(String address, String username,
+            String password) throws SnmpException {
+        
+        ProtocolOptions protocolOptions = new UDPProtocolOptions(address);
+        
+        SnmpSession session = new SnmpSession(api);
+        session.setVersion(SnmpAPI.SNMP_VERSION_3);
+        session.setProtocolOptions(protocolOptions);
+        session.open();
+        
+        USMUtils.init_v3_parameters(username, null, USMUserEntry.MD5_AUTH,
+            password, null, protocolOptions, session, true);
+        
+        return session;
+    }
+}
+
+
