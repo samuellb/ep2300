5a5b9651044ec871f015724f54993f366f920c81
Linus Wallgren <linus.wallgren@gmail.com>
2011-09-28 01:16:42 +0200

    Topology traverses the routers and tries to find all

--- src/ep2300/Topology.java
+++ src/ep2300/Topology.java
@@ -1,10 +1,21 @@
 package ep2300;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
+import com.adventnet.snmp.snmp2.SnmpAPI;
 import com.adventnet.snmp.snmp2.SnmpClient;
 import com.adventnet.snmp.snmp2.SnmpException;
+import com.adventnet.snmp.snmp2.SnmpIpAddress;
 import com.adventnet.snmp.snmp2.SnmpOID;
 import com.adventnet.snmp.snmp2.SnmpPDU;
 import com.adventnet.snmp.snmp2.SnmpSession;
+import com.adventnet.snmp.snmp2.SnmpVar;
+import com.adventnet.snmp.snmp2.SnmpVarBind;
+import com.adventnet.snmp.snmp2.UDPProtocolOptions;
 
 /*
  * TODO In the constructor, create an initial session and request the route
@@ -20,41 +31,90 @@ import com.adventnet.snmp.snmp2.SnmpSession;
  */
 public class Topology implements SnmpClient
 {
-    private SnmpOID discoverOID;
+    private final SnmpOID discoverOID = new SnmpOID(".1.3.6.1.2.1.4.21.1.7"); // ipRouteNextHop
+    Map<String, Set<SnmpIpAddress>> neighbors = new HashMap<String, Set<SnmpIpAddress>>();
 
     public Topology()
     {
-        discoverOID = new SnmpOID(".1.3.6.1.2.1.4.21.1.7"); // ipRouteNextHop
+        probe("192.168.1.10");
+        System.out.println("Topology constructor done");
+    }
 
+    private void probe(String ip)
+    {
+        SnmpSession session = null;
         try {
-            UDPSnmpV3.createSession("192.168.1.10"); // Begin here
-
+            session = UDPSnmpV3.createSession(ip); // Begin here
         }
         catch (SnmpException e) {
-            System.err.println("Could not start initial session, aborting");
-            System.exit(1); // TODO Do proper shutdown
+            System.err.println("Could not start session to ip " + ip);
+            e.printStackTrace();
+        }
+        if (session != null) { // Should always happen
+            int id = session.addSnmpClientWithID(this);
+            SnmpPDU pdu = new SnmpPDU();
+            pdu.setCommand(SnmpAPI.GETBULK_REQ_MSG);
+            pdu.setClientID(id);
+            
+            // TODO below nescesary? should be handled by someone qualified
+            pdu.setMaxRepetitions(30);
+
+            pdu.addNull(discoverOID);
+            try {
+                session.send(pdu);
+            }
+            catch (SnmpException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
 
     @Override
     public boolean authenticate(SnmpPDU pdu, String community)
     {
-        // TODO Auto-generated method stub
-        return false;
+        return true; // Because we already are authenticated
     }
 
     @Override
     public boolean callback(SnmpSession session, SnmpPDU pdu, int requestID)
     {
-        // TODO Auto-generated method stub
-        return false;
+
+        if (pdu.getErrstat() != 0) { // FIXME is this state reachable?
+            System.out.println("A request has failed:");
+            System.out.println(pdu.getError());
+            return true; // No further processing is needed
+        }
+        else {
+            Vector bindings = pdu.getVariableBindings();
+            UDPProtocolOptions opt = (UDPProtocolOptions) session
+                    .getProtocolOptions();
+            String router = opt.getRemoteAddress().getCanonicalHostName();
+            Set<SnmpIpAddress> nextHops = neighbors.get(router);
+            if (nextHops == null) {
+                System.out.println("New router discovered: \t" + router);
+                nextHops = new HashSet<SnmpIpAddress>();
+                neighbors.put(router, nextHops);
+            }
+            for (int i = 0; i < bindings.size(); ++i) {
+                SnmpVar var = ((SnmpVarBind) bindings.get(i)).getVariable();
+                if (var instanceof SnmpIpAddress) {
+                    SnmpIpAddress ip = (SnmpIpAddress) var;
+                    nextHops.add(ip);
+                    if (neighbors.get(ip.toString()) == null) {
+                        // Not yet probed
+                        probe(ip.toString());
+                    }
+                }
+            }
+            return true; // done processing PDU
+        }
     }
 
     @Override
     public void debugPrint(String debugOutput)
     {
-        // TODO Auto-generated method stub
-
+        System.err.println("debuging");
     }
 
 }
