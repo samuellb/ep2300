c4a3ba657ca51a9bb8b78ffb3dbec4aee4e60db4
Samuel Lidén Borell <samuel@slbdata.se>
2011-09-27 21:53:24 +0200

    Experiment with getbulk and ipRouteNextHop

--- src/ep2300/Test.java
+++ src/ep2300/Test.java
@@ -13,19 +13,29 @@ public class Test {
         SnmpSession session = UDPSnmpV3.createSession("192.168.1.10",
             "2G1332_student", "netmanagement");
         
+        // .1.3.6.1.2. <-- this is implicit if the OID doesn't start with a .
+        // .1.3.6.1.2.1.4.21.1.7 = ipRouteNextHop
         SnmpPDU pdu = new SnmpPDU();
-        pdu.setCommand(SnmpAPI.GET_REQ_MSG);
-        pdu.addNull(new SnmpOID(".1.3.6.1.2.1.1.1.0"));
         pdu.setUserName("2G1332_student".getBytes());
+        pdu.setCommand(SnmpAPI.GETBULK_REQ_MSG);
+        pdu.setMaxRepetitions(30);
+        pdu.setNonRepeaters(0);
+        
+        pdu.addNull(new SnmpOID(".1.3.6.1.2.1.4.21.1.7"));
+        
         SnmpPDU response_pdu = session.syncSend(pdu);
-        if(response_pdu == null) {
+        if (response_pdu == null) {
              System.out.println("The Request has timed out.");
+        } else if (response_pdu.getErrstat() != 0) {
+            System.out.println("The request failed.");
+            System.out.println(response_pdu.getError());
         } else {
-            System.out.println("got response!");
+            System.out.println("got response of ipRouteNextHop!");
             System.out.println(response_pdu.printVarBinds());
         }
         
-        System.out.println("done");
+        System.out.println("done (code v 3)");
+
     }
     
 }
